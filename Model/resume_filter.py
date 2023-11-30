467399924a01e87178c8c716b41f82566cc4ff59
Jashanjot Singh <jashanjotsingh@my.bcit.ca>
2023-11-30 10:14:34 -0800

    Turned Python Model into Flask App

--- Model/resume_filter.py
+++ Model/resume_filter.py
@@ -1,14 +1,32 @@
+from flask import Flask, request, jsonify
 import docx2txt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-job_description = docx2txt.process("job_description.docx")
-resume = docx2txt.process("resume.docx")
+app = Flask(__name__)
 
-content = [resume, job_description]
-cv = CountVectorizer()
+def calculate_similarity(job_description, resume):
+    content = [resume, job_description]
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(content)
+    similarity_matrix = cosine_similarity(count_matrix)
+    return similarity_matrix[1][0].round(2) * 100
 
-count_matrix = cv.fit_transform(content)
-simirlarity_matrix = cosine_similarity(count_matrix)
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    if 'job_description' not in request.files or 'resume' not in request.files:
+        return 'Missing files', 400
 
-print("Resume matches by : " + str(simirlarity_matrix[1][0].round(2)*100) + "%")
+    job_description_file = request.files['job_description']
+    resume_file = request.files['resume']
+
+    job_description = docx2txt.process(job_description_file)
+    resume = docx2txt.process(resume_file)
+
+    similarity_score = calculate_similarity(job_description, resume)
+    return jsonify({'similarity': similarity_score})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
