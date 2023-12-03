3fab77fc73d0194914d2ee11678aaba708ef824c
Jashanjot Singh <jashanjotsingh@my.bcit.ca>
2023-12-03 10:25:25 -0800

    Handling multiple resumes and ranking them

--- FrontEnd/js/home.js
+++ FrontEnd/js/home.js
@@ -1,5 +1,6 @@
-
 let resumes = [];
+let resumeResults = [];
+
 
 function addResume(resume) {
     resumes.push(resume);
@@ -12,8 +13,7 @@ function addResumeToList() {
 
     for (let i = 0; i < resumeInput.files.length; i++) {
         const resume = resumeInput.files[i];
-        
-        // Validate file type and size here (example: .docx files and size < 5MB)
+
         if (resume.name.endsWith('.docx') && resume.size < 5000000) {
             addResume(resume);
             addedResumesDiv.innerHTML += `<p>${resume.name}</p>`;
@@ -24,11 +24,9 @@ function addResumeToList() {
     }
 
     if (!hasValidFiles) {
-        // No valid files added
         return;
     }
 
-    // Clear the file input to allow selecting the same files again
     resumeInput.value = null;
 }
 
@@ -40,63 +38,92 @@ function submitForm() {
         return;
     }
 
-    // Process the job application data and resumes
     console.log('Job Description:', jobDescription);
     console.log('Resumes:', resumes);
 
-    // Send data to server
-    sendDataToServer(jobDescription, resumes);
+    resumes.forEach(resume => {
+        sendDataToServer(jobDescription, resume);
+    });
 
-    // Reset the resumes array for the next submission
     resumes = [];
 }
 
-
-
-function getUsage(){
-
-}
-
-function sendDataToServer(jobDescription, resumes) {
+function sendDataToServer(jobDescription, resume) {
     let formData = new FormData();
-    formData.append('job_description', jobDescription); // Ensure the name matches with Flask's expected key
-
-    // Assuming you want to send only the first resume for simplicity
-    if (resumes.length > 0) {
-        formData.append('resume', resumes[0]);
-    }
+    formData.append('job_description', jobDescription);
+    formData.append('resume', resume);
 
     fetch('http://127.0.0.1:5000/upload', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-               // UI Update for Success
-               document.getElementById('successMessage').style.display = 'block';
-               document.getElementById('successMessage').textContent = 'Success: The similarity score is ' + data.similarity + '%';
-       
-               // Hide error message in case it was previously shown
-               document.getElementById('errorMessage').style.display = 'none';
-       
-               // You might also want to update other parts of your UI, such as clearing the form or disabling the submit button
-               // document.getElementById('jobApplicationForm').reset();
-           })
-           .catch(error => {
-               console.error('Error:', error);
-       
-               // UI Update for Error
-               document.getElementById('errorMessage').style.display = 'block';
-               document.getElementById('errorMessage').textContent = 'Error: ' + error.message;
-       
-               // Hide success message in case it was previously shown
-               document.getElementById('successMessage').style.display = 'none';
-           });
-       }
-       
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            resumeResults.push({ name: resume.name, similarity: data.similarity });
+            displayResults();
+            document.getElementById('errorMessage').style.display = 'none';
+        })
+        .catch(error => {
+            console.error('Error:', error);
 
-function goLogin(){
+            document.getElementById('errorMessage').style.display = 'block';
+            document.getElementById('errorMessage').textContent = 'Error: ' + error.message;
+            document.getElementById('successMessage').style.display = 'none';
+        });
+}
+
+function displayResults() {
+    if (resumeResults.length > 0) {
+        let defaultMessage = document.getElementById('defaultMessage');
+        if (defaultMessage) {
+            defaultMessage.remove();
+        }
+    }
+
+    resumeResults.sort((a, b) => b.similarity - a.similarity);
+
+    let resultsContainer = document.getElementById('resultsContainer');
+    resultsContainer.innerHTML = '';
+
+    resumeResults.forEach(result => {
+        updateUIWithResumeResult(result.name, result.similarity);
+    });
+}
+
+
+function updateUIWithResumeResult(name, similarity) {
+    let resultsContainer = document.getElementById('resultsContainer');
+    let roundedSimilarity = parseFloat(similarity).toFixed(2);
+
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('card', 'mb-3', 'border-success');
+
+    let cardHeader = document.createElement('h5');
+    cardHeader.classList.add('card-header', 'bg-success', 'text-white');
+    cardHeader.textContent = `Resume: ${name}`;
+
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'bg-light');
+
+    let similarityText = document.createElement('p');
+    similarityText.classList.add('card-text');
+    similarityText.innerHTML = `Similarity: <strong class="${getSimilarityColor(roundedSimilarity)}">${roundedSimilarity}%</strong>`; // Color based on similarity score
+
+    cardDiv.appendChild(cardHeader);
+    cardDiv.appendChild(cardBody);
+    cardBody.appendChild(similarityText);
+
+    resultsContainer.appendChild(cardDiv);
+}
+
+function getSimilarityColor(similarity) {
+    if (similarity > 60) return 'text-success';
+    else if (similarity > 50) return 'text-warning';
+    return 'text-danger';
+}
+
+function goLogin() {
     const choice = confirm("Are you sure you want to logout?");
-    if(choice) window.location.href = 'Login.html';
+    if (choice) window.location.href = 'Login.html';
 }
